97b9587fe141f058641a01d42b9d52fba6bf2d4c
Tom Donahue <dasginganinja@gmail.com>
2023-07-20 00:12:26 -0400

    Update launcher.go

--- launcher.go
+++ launcher.go
@@ -5,33 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 )
-
-func FindDrupalRoot(path string) (string, error) {
-	// Start traversing from the provided path to the root directory
-	currentDir := path
-	for {
-		// Check if the vendor/bin/drush directory exists in the current directory
-		drushDir := filepath.Join(currentDir, "vendor", "bin", "drush")
-		if _, err := os.Stat(drushDir); err == nil {
-			// Drupal root found, traverse one level up to get the root directory
-			drupalRoot := filepath.Dir(currentDir)
-			return drupalRoot, nil
-		}
-
-		// Move one level up the directory tree
-		parentDir := filepath.Dir(currentDir)
-		// If we reached the root directory, stop traversing
-		if parentDir == currentDir {
-			break
-		}
-		currentDir = parentDir
-	}
-
-	// Drupal root not found in the entire directory tree
-	return "", fmt.Errorf("Drupal root not found")
-}
 
 func main() {
 	// Parse command-line flags
